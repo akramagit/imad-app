d47e9d0622f3c99e869defb88e7e48a5874f00fb
akramagit <30863067+akramagit@users.noreply.github.com>
2017-08-14 22:50:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
