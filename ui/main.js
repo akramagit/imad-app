532346da152493bb13eed4061d8d058af9917cac
akramagit <30863067+akramagit@users.noreply.github.com>
2017-08-17 00:01:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,8 @@
 console.log('Loaded!');
+// Change the text of the main div
+var element = document.getElementById('main-text');
 // Move the image
 var img = document.getElementById('madi');
 img.onclick = function () {
     img.style.marginLeft = '100px';
-
 };
