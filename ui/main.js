c825a21b0ef09c8ad018461841093576abc183c7
akramagit <30863067+akramagit@users.noreply.github.com>
2017-08-17 00:04:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log('Loaded!');
 // Change the text of the main div
 var element = document.getElementById('main-text');
+element.innerHTML = 'New value';
 // Move the image
 var img = document.getElementById('madi');
 img.onclick = function () {
