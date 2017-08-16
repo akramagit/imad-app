c3423d8514fa15fa643291af15b95b60c4e8c196
akramagit <30863067+akramagit@users.noreply.github.com>
2017-08-17 00:31:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 // Move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
+    var interval = msetInterval(moveRight, 100);
     img.style.marginLeft = '100px';
 };
